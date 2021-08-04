ef8ad509d433749be06e0c5002075c5a89ac85b5
shiiba <dndtuan.ly.cr@gmail.com>
2021-08-04 21:47:36 +0700

    user ui

---
+++ components/user/userpage/inputfield.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
+function InputField(props: {
+  name: string;
+  editable: boolean;
+  data: string;
+  multiline: boolean;
+}) {
+  const { handleSubmit } = useForm();
+  const [data, setData] = useState(props.data);
+  const [isActive, setIsActive] = useState(false);
+  const onSubmit = () => {
+    setIsActive(!!!isActive);
+    console.log(data);
+    // Push Api
+  };
+  const handleChange = (e) => {
+    setData(e.target.value);
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/4">
+          <label
+            className="block text-gray-500 font-bold md:text-center mb-1 md:mb-0 pr-4"
+            htmlFor="inline-full-name"
+          >
+            {props?.name}:
+          </label>
+        </div>
+        <div className="md:w-2/3">
+          {isActive ? (
+            <input
+              className="appearance-none border-2 outline-none bg-white border-purple-500 rounded w-5/6 py-2 px-4 text-gray-700 leading-tight"
+              id={props?.name}
+              type="text"
+              defaultValue={data}
+              onChange={handleChange}
+            />
+          ) : (
+            <input
+              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-5/6 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+              id={props?.name}
+              type="text"
+              disabled
+              value={data}
+              defaultValue={isActive ? data : undefined}
+            />
+          )}
+        </div>
+        <div>
+          {props?.editable ? (
+            <button
+              type="submit"
+              className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+            >
+              {isActive ? "Submit" : "Edit"}
+            </button>
+          ) : (
+            <button disabled />
+          )}
+        </div>
+      </div>
+    </form>
+  );
+}
+
+export default InputField;
---
+++ components/user/userpage/userpage.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import InputField from "components/user/userpage/inputfield";
+
+type classInfo = {
+  name: string;
+  subject: string;
+  teacher: string;
+  description: string;
+  //   Note: Có thể sẽ để description là list để hiện nhiều dòng
+};
+
+const UserPage = function () {
+  const props = {
+    name: "A",
+    email: "B",
+    id: "C",
+    description: "D",
+  };
+  return (
+    <div>
+      <div className="container items-center pt-44">
+        <InputField name="Tên" editable data={props.name} multiline={false} />
+        <InputField
+          name="Email"
+          editable
+          data={props.email}
+          multiline={false}
+        />
+        <InputField
+          name="Mã số sinh viên"
+          editable
+          data={props.id}
+          multiline={false}
+        />
+        <InputField
+          name="Tiểu sử"
+          editable
+          data={props.description}
+          multiline
+        />
+      </div>
+    </div>
+  );
+};
+
+export default UserPage;
