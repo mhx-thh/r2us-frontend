5664910bd3c28ef67b65456f7edcfc8dc642e2a7
phamvmnhut <phamvmnhut@gmail.com>
2021-08-04 20:26:37 +0700

    home page v1

--- api/NewClassAPI.ts
+++ api/NewClassAPI.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { apiV1, config, get } from "api/generic";
 
 const NewClassAPI = {
-  get: function (token: string) {
+  get: function () {
     const url = "/group/class";
     return get(url, "");
   },
