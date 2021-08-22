97ee01294c0bd256459f3b4179f6803d95b329c0
phamvmnhut <phamvmnhut@gmail.com>
2021-08-22 12:41:57 +0700

    update build

--- next.config.js
+++ next.config.js
@@ -1,20 +1,8 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
-
-module.exports = withBundleAnalyzer({
-  target: "serverless",
-  publicRuntimeConfig: {
-    // add your public runtime environment variables here with NEXT_PUBLIC_*** prefix
-  },
-  webpack: (config) => {
-    // extend your webpack configuration here
-    return config;
-  },
+module.exports = {
+  target: "experimental-serverless-trace",
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-});
+};
